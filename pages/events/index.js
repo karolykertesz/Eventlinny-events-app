df0d9c4120e17d95b12b32092fe3b8c147ad06ce
kertesz Karoly <kforcegroup@hotmail.com>
2021-04-22 02:38:19 +0200

    added firebase, getStaticProps function,created server side rendering

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,20 +1,72 @@
-import { getAllEvents } from "../../data";
+import { useEffect, useState } from "react";
 import EventList from "../../components/EventList";
 import EventSearch from "../../components/event-search.jsx";
 import { useRouter } from "next/router";
-const AllEvents = () => {
-  const allE = getAllEvents();
+import useSWR from "swr";
+
+const AllEvents = ({ eventList }) => {
+  const { data, error } = useSWR(
+    "https://next-events-309cd-default-rtdb.firebaseio.com/events.json"
+  );
+  const [fetched, setFetched] = useState(eventList);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const onSelected = (y, m) => {
     const path = `/events/${y}/${m}`;
     router.push(path);
   };
+  useEffect(() => {
+    setLoading(true);
+    if (data) {
+      let events = [];
+      for (let key in data) {
+        events.push({
+          id: key,
+          title: data[key].title,
+          description: data[key].description,
+          location: data[key].location,
+          image: data[key].image,
+          isFeatured: data[key].isFeatured,
+        });
+      }
+      setFetched(events);
+      setLoading(false);
+    }
+  }, []);
+  if (loading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <EventSearch onSelected={onSelected} />
-      <EventList items={allE} />
+      {fetched && <EventList items={fetched} />}
     </div>
   );
 };
 
 export default AllEvents;
+
+const getStaticProps = async () => {
+  const response = await fetch(
+    "https://next-events-309cd-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = response.json();
+  let eventList = [];
+  if (data) {
+    for (let key in data) {
+      eventList.push({
+        id: key,
+        title: data[key].title,
+        description: data[key].description,
+        location: data[key].location,
+        image: data[key].image,
+        isFeatured: data[key].isFeatured,
+      });
+    }
+  }
+  return {
+    props: {
+      eventList,
+    },
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,62 @@
+import { useState, useEffect } from "react";
 import { getFeaturedEvents } from "../data";
 import EventList from "../components/EventList";
+import useSWR from "swr";
 
-const Home = () => {
-  const featuredEvents = getFeaturedEvents();
+const Home = ({ eventss }) => {
+  const [fetched, useFetched] = useState(eventss);
+  const { data, error } = useSWR(
+    "https://next-events-309cd-default-rtdb.firebaseio.com/events.json"
+  );
+  useEffect(() => {
+    if (data) {
+      const event = [];
+      for (let key in data) {
+        event.push({
+          id: key,
+          title: data[key].title,
+          description: data[key].description,
+          location: data[key].location,
+          image: data[key].image,
+          isFeatured: data[key].isFeatured,
+        });
+      }
+      useFetched(event);
+    }
+  }, []);
+
   return (
     <div>
-      <EventList items={featuredEvents} />
+      <EventList items={fetched} />
     </div>
-  );    
+  );
 };
 export default Home;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-events-309cd-default-rtdb.firebaseio.com/events.json"
+  );
+  const data = await response.json();
+  const events = [];
+
+  for (let key in data) {
+    events.push({
+      id: key,
+      title: data[key].title,
+      description: data[key].description,
+      location: data[key].location,
+      image: data[key].image,
+      isFeatured: data[key].isFeatured,
+    });
+  }
+  let filtered;
+  if (events.length > 0) {
+    filtered = events.filter((ev) => ev.isFeatured === true);
+  }
+  return {
+    props: {
+      eventss: filtered,
+    },
+  };
+}
