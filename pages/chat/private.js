d01f2896b247d2a7cc692894597b81483d9a7edd
karolykertesz <kforcegroup@hotmail.com>
2021-07-21 00:01:32 +0200

    added private check CSRF validation,user banned validation

--- pages/chat/private.js
+++ pages/chat/private.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { useRedirect } from "../../helpers/validatehelp";
 import useBanned from "../../helpers/checkBanned";
 import classes from "../../components/UI/ui-modules/private.chat.login.module.css";
@@ -7,6 +7,7 @@ import { useRouter } from "next/router";
 import firebase from "firebase";
 import validate from "validate.js";
 import { constraints } from "../../helpers/validators/privatechat";
+import Loader from "../../components/UI/loader";
 const Private = () => {
   useRedirect();
   const isBanned = useBanned();
@@ -15,48 +16,81 @@ const Private = () => {
   const [room, setRoom] = useState();
   const [password, setPass] = useState();
   const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
   const formRef = useRef();
-  if (isBanned) {
-    return (
-      <div classname={classes.banned}>You have Been banned from chat!!</div>
-    );
-  }
+  const fetchTokken = useCallback(async () => {
+    const mess = await fetch("/api/users/helpers/csrf");
+    const { secret } = await mess.json();
+    formRef.current.value = await secret;
+  }, []);
   const formsubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
     const value = validate({ room, password }, constraints);
     if (value) {
       return setError("Invalid Input values!!!");
     }
+    const mess = await fetch("/api/users/helpers/csrf", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ secret: formRef.current.value }),
+    });
+    const status = await mess.status;
+    if (status !== 200) {
+      setLoading(false);
+      return setError("Please refresh the page");
+    }
     const baseRef = firebase
       .firestore()
       .collection("private_chat_users")
       .doc(room.trim().toLowerCase());
-
+    const secRef = firebase
+      .firestore()
+      .collection("private_chat")
+      .doc(room.trim().toLowerCase());
     await baseRef.get().then(async (doc) => {
       if (doc.exists) {
         const verified = await doc.data().verified;
-        if (verified.indexOf(user.email) > -1) {
-          return firebase
-            .firestore()
-            .collection("private_chat")
-            .doc(room.trim().toLowerCase())
-            .get()
-            .then((cd) => {
-              const pass = cd.data().password;
-              if (pass === password.toString()) {
-                router.push(`/chat/pr?chi=${room}`);
-              } else {
-                setError("invalid Password");
-              }
-            });
+        if (verified && verified.indexOf(user && user.email) > -1) {
+          return secRef.get().then(async (cd) => {
+            const pass = cd.data().password;
+            if (pass === password.toString()) {
+              await secRef.update({
+                active_users: firebase.firestore.FieldValue.arrayUnion(
+                  user.uid
+                ),
+              });
+              await setLoading(false);
+              await router.push(`/chat/pr?chi=${room}`);
+            } else {
+              setLoading(false);
+              setError("invalid Password");
+            }
+          });
         } else {
+          setLoading(false);
           return setError("Sorry Your Email wasn't verified ,yet!");
         }
       } else {
+        setLoading(false);
         return setError("Invalid Room Name");
       }
     });
   };
+
+  useEffect(() => {
+    fetchTokken();
+  }, [fetchTokken]);
+  if (isBanned) {
+    return (
+      <div classname={classes.banned}>You have Been banned from chat!!</div>
+    );
+  }
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <div className={classes.top}>
       <div className={classes.cover}>
